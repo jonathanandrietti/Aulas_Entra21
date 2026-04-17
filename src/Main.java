557ef322650d3478595183c245f41ecb91505f80
matrixtato <42388326+matrixtato@users.noreply.github.com>
2026-04-17 20:40:40 -0300

    Adicionado mensagens
    Editado código

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome! \n");
+        System.out.println("Olá e Bem Vindo! \n");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
